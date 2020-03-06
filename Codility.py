35084d536b8082a6f361da18d2fb92117483b976
David Boland <dboland77@gmail.com>
2020-03-06 12:31:02 +0000

    Checking git differences with VsCode git

--- Codility.py
+++ Codility.py
@@ -1,20 +1,17 @@
 import math
 import os
 import sys
-
 import requests
 
 # print(sys.version)
-# print(sys.executable)
+# print(sys.executable
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
-
-
-# r = requests.get("https://daveboland.co.uk")
-# print(r.status_code)
+r = requests.get("https://daveboland.co.uk")
+print(r.status_code)
 
 name = input("Your name? ")
 print("Hello, ", name)
